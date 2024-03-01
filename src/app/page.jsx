aa5158566d5f1a97059f073a45887f6e6e4fa401
REZA-OJAGHI-DRO <frontenddro@gmail.com>
2024-03-01 10:18:39 +0330

    'reza'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import S1 from './section/section1'
 import S2 from './section/section2'
 import S3 from './section/section3'
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef ,useState} from "react";
 
 export default function Home() {
   const ss=useStore((state)=>state.ss)
@@ -19,7 +19,7 @@ export default function Home() {
   useEffect(() => {
     const handleSmoothScroll = (e) => {
       const delta = e.deltaY; 
-      scrollRef.current.scrollTop += delta * 0.2; 
+      scrollRef.current.scrollTop += delta * .5; 
       e.preventDefault(); 
     };
 
@@ -29,6 +29,24 @@ export default function Home() {
       scrollRef.current.removeEventListener('wheel', handleSmoothScroll);
     };
   }, []);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0 && !isScrolled) {
+        setIsScrolled(true);
+      } else if (window.scrollY === 0 && isScrolled) {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isScrolled]);
+
   function scroll(e){
     const st=e.target.scrollTop
     const sec3Ott =(x.current.offsetTop)
@@ -47,12 +65,14 @@ export default function Home() {
   }
   return (
     <main className="w-[100%] overflow-hidden h-[100vh]">
+      <div className={isScrolled ? 'smooth-scroll-active' : ''}>
       <div ref={scrollRef} onScroll={()=>scroll(event)} className="w-[calc(100%+25px)] h-[100vh] overflow-y-scroll relative">
-        {/* <div className="w-[5rem] h-[3rem] bg-slate-800 text-white fixed top-0 z-50">{Math.floor(sT)}</div> */}
+        <div className="w-[5rem] h-[3rem] bg-slate-800 text-white fixed top-0 z-50">{Math.floor(sT)}</div>
         <S3/>
         <S2/>
         <S1/>
         <div ref={x} className="w-[100%] h-[100vh]"></div>
+      </div>
       </div>
     </main>
   );
